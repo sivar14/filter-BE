ea7702fbe1da16226d76b51b4ed14460c72c5fb3
T-Prakash2002 <prakasht1405@gmail.com>
2024-07-13 14:22:38 +0530

    final step added

--- index.js
+++ index.js
@@ -1,69 +1,76 @@
-const express= require("express");
-const users= require("./sample.json");
-const cors= require("cors");
-const fs= require("fs");
+const express = require("express");
+const users = require("./sample.json");
+const cors = require("cors");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
-const app= express();
+
+
+const app = express();
 app.use(express.json());
-const port=8000;
+const port = 8000;
 
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 
 
 //display all users
-app.get("/Getusers",(req, res)=>{
-return res.json(users);
+app.get("/Getusers", (req, res) => {
+    return res.json(users);
 });
 
 //delete users
-app.delete("/users/:id",(req, res)=>{
-    let id= Number(req.params.id);
-    let filteredUsrs= users.filter((user)=>user.id!==id);
-    fs.writeFile("./sample.json", JSON.stringify(filteredUsrs),(err,data)=>{
+app.delete("/Deleteusers/:id", (req, res) => {
+
+    let {id} = req.params;
+    let filteredUsrs = users.filter((user) => user.id !== id);
+    fs.writeFile("./sample.json", JSON.stringify(filteredUsrs), (err, data) => {
         return res.json(filteredUsrs);
     });
-    });
+
+    
+});
 
 // add users
 
-app.post("/Addusers",(req, res)=>{
-    let{name, age, city}= req.body;
-    if(!name|| !age|| !city){
-      res.status(400).send({message: "all fields required"}) ; 
+app.post("/Addusers", (req, res) => {
+    let { name, age, city } = req.body;
+    if (!name || !age || !city) {
+        res.status(400).send({ message: "all fields required" });
     }
-    let users=[];
-    let id= date.now();
-    users.push({id, name, age, city});
 
-    fs.writeFile("./sample.json", JSON.stringify(users),(err,data)=>{
-        return res.json({message:"added successfully"});
-    }); 
+
+    users.push({ name, age, city, id: uuidv4() });
+
+    fs.writeFile("./sample.json", JSON.stringify(users), (err, data) => {
+        return res.json({ message: "added successfully" });
+    });
 });
+
 
 //update
 
-app.put("/users/:id",(req, res)=>{
-    let id= Number(req.params.id);
-    let{name, age, city}= req.body;
-    if(!name|| !age|| !city){
-      res.status(400).send({message: "all fields required"}) ; 
+app.put("/users/:id", (req, res) => {
+    let {id }=req.params;
+    let { name, age, city } = req.body;
+    if (!name || !age || !city) {
+        res.status(400).send({ message: "all fields required" });
     }
-    let index=users.findIndex((user)=>user.id==id);
-    users.splice(index, 1, {...req.bod});
+    let index = users.findIndex((user) => user.id == id);
+    users.splice(index, 1, { ...req.body });
 
-    fs.writeFile("./sample.json", JSON.stringify(users),(err,data)=>{
-        return res.json({message:"updated successfully"});
-    }); 
+    fs.writeFile("./sample.json", JSON.stringify(users), (err, data) => {
+        return res.json({ message: "updated successfully" });
+    });
 });
 
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`app is runnig on port :${port}`)
 });
